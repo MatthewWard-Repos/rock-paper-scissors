4839345dda095ee4bbba24d29ea4d8ac9648a5be
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-09-26 15:46:46 +0100

    Remove original playRound function

--- script.js
+++ script.js
@@ -10,58 +10,6 @@ let humanScore = 0;
 let computerScore = 0;
 let round = 1;
 
-// const playGame = function () {
-//   const playRound = function () {
-//     const computer = getComputerChoice();
-//     const human = getHumanChoice(target);
-//     let winner = human;
-//     let result;
-//     endRound = function (winner, result) {
-//       if (human === computer) {
-//         null;
-//       } else {
-//         winner === human ? humanScore++ : computerScore++;
-//       }
-
-//       console.log(
-//         `You ${result} round ${round} of 5! ${human} ${
-//           human === computer ? "vs" : winner === human ? "beats" : "beaten by"
-//         } ${computer},${
-//           human === computer ? " No winner" : ""
-//         } ${humanScore}/${computerScore}`
-//       );
-//       round++;
-//     };
-
-//     if (human === computer) {
-//       result = "drew";
-//     } else {
-//       (human === "rock" && computer === "scissors") ||
-//       (human === "paper" && computer === "rock") ||
-//       (human === "scissors" && computer === "paper")
-//         ? ((winner = human), (result = "won"))
-//         : ((winner = computer), (result = "lost"));
-//     }
-//     endRound(winner, result);
-//   };
-
-//   // for (let i = 1; i <= 5; i++) {
-//   //   playRound();
-//   // }
-
-//   if (humanScore === computerScore) {
-//     result = "drew";
-//   } else {
-//     humanScore > computerScore ? (result = "won") : (result = "lost");
-//   }
-//   gameOver = function () {
-//     console.log(
-//       `You ${result} the game! Final Score ${humanScore}/${computerScore}`
-//     );
-//   };
-//   gameOver();
-// };
-
 const playRound2 = function (choice) {
   const computer = getComputerChoice();
   const human = choice;
