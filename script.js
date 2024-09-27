879547df9f3b7c3a85a8ddd13c913f45cdb5e7f3
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-09-27 11:32:53 +0100

    Fixed winner error and refactored functions.
    Fixed winner error due to checking for
    un-capitalised inputs.
    
    Refactored endRound() into several functions called
    by endRound().

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
 const getComputerChoice = function () {
   const choiceC = Math.floor(Math.random() * 3);
   return choiceC === 1 ? "Rock" : choiceC === 2 ? "Paper" : "Scissors";
 };
-
-let humanScore = 0;
-let computerScore = 0;
-let round = 1;
+const init = function () {
+  humanScore = 0;
+  computerScore = 0;
+  round = 1;
+};
+init();
 
 const playRound2 = function (choice) {
   const computer = getComputerChoice();
   const human = choice;
   let winner = human;
+  let result;
   const textBox = document.querySelector(".results");
   const textHuman = document.querySelector(".human");
   const textComputer = document.querySelector(".computer");
 
   endRound = function (winner, result) {
     gameOver = function () {
-      textBox.textContent = `You ${result} the game! Final Score ${humanScore}/${computerScore}`;
-      humanScore = 0;
-      computerScore = 0;
-      round = 1;
+      textBox.textContent = `You ${result} the game!`;
+      init();
     };
-    if (human === computer) {
-      null;
-    } else {
-      winner === human ? humanScore++ : computerScore++;
-    }
-    textHuman.textContent = humanScore;
-    textComputer.textContent = computerScore;
-    if (!(humanScore === 5 || computerScore === 5)) {
-      textBox.textContent = `You ${result} round ${round}! ${human} ${
-        human === computer ? "vs" : winner === human ? "beats" : "beaten by"
-      } ${computer}${human === computer ? " - No winner" : ""} `;
+    getWinnerResult = function () {
+      if (human === computer) {
+        result = "drew";
+      } else {
+        (human === "Rock" && computer === "Scissors") ||
+        (human === "Paper" && computer === "Rock") ||
+        (human === "Scissors" && computer === "Paper")
+          ? ((winner = human), (result = "won"))
+          : ((winner = computer), (result = "lost"));
+      }
+    };
+    updateScore = function () {
+      if (human === computer) {
+        null;
+      } else {
+        winner === human ? humanScore++ : computerScore++;
+      }
+      textHuman.textContent = humanScore;
+      textComputer.textContent = computerScore;
+    };
 
-      round++;
-    } else {
-      humanScore > computerScore ? (result = "won") : (result = "lost");
-      gameOver();
-    }
+    printResult = function () {
+      if (!(humanScore === 5 || computerScore === 5)) {
+        textBox.textContent = `You ${result} round ${round}!
+         ${human} ${
+          human === computer ? "vs" : winner === human ? "beats" : "beaten by"
+        } ${computer}
+        ${human === computer ? " - No winner" : ""} `;
+
+        round++;
+      } else {
+        humanScore > computerScore ? (result = "won") : (result = "lost");
+        gameOver();
+      }
+    };
+    getWinnerResult();
+    updateScore();
+    printResult();
   };
-  if (human === computer) {
-    result = "drew";
-  } else {
-    (human === "rock" && computer === "scissors") ||
-    (human === "paper" && computer === "rock") ||
-    (human === "scissors" && computer === "paper")
-      ? ((winner = human), (result = "won"))
-      : ((winner = computer), (result = "lost"));
-  }
+
   endRound(winner, result);
 };
 // playGame();
@@ -60,4 +77,3 @@ buttons.addEventListener("click", function (e) {
     playRound2(target.toUpperCase().slice(0, 1) + target.slice(1));
   }
 });
-// console.log("target".toUpperCase().slice(0, 1) + "target".slice(1));
